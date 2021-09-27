4767530ceb355be9829cea4962831494b55a7d92
erinszabo <55733370+erinszabo@users.noreply.github.com>
2021-09-26 19:51:01 -0700

    aah much better

--- main.rb
+++ main.rb
@@ -1,27 +1,34 @@
 
 class Bike
-    # ...
-  end
+    # no need for RedBicycles when we can have bicycles that are red
+    # plus weird to have bike and bycicle
+    # AND camelCase when Ruby is buddies with Python and we_prefer_snake_case
+
+    WEIGHT_INCREMENT = 10 # AMOUNT is a useless variable name.
+    # The all caps was handy though since it is a constant.
   
-  class RedBicycle < Bike
-  
-    AMOUNT = 10
-  
-    def initialize(a, b, c)
-      @height = a
-      @weight = b
-      @color = c
+    # plain letters are not useful variable names...
+    def initialize(height, weight, color)
+      @height = height
+      @weight = weight
+      @color = color
     end
   
-    def getColor
+    # lets not name the same task 2 different things
+    def get_color
       @color
     end
   
-    def retreive_height
+    def get_height
       @height
     end
   
-    def change_weight
-      @weight -= AMOUNT
+    # 'change' is not as specific as 'reduce'
+    # 'decrement' could have worked but 'weight reduction' makes more sense in this context
+    def reduce_weight 
+      @weight -= WEIGHT_INCREMENT
+      # increment could be confusing and imply an increase rather than a decrease 
+      # but it is being reduced by increments of 10 so it makes sense??? 
+      # It's the best of the names that came to mind.
     end
-  end
+end
